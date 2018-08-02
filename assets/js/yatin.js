e3b7ba479d84551e195aa17525dff976a7f456c6
Yatin Ag <yatin.agarwal.9@gmail.com>
2018-08-02 07:48:05 -0400

    modem-18-upload

---
+++ assets/js/yatin.js
@@ -0,0 +1,189 @@
+/*!
+ * Vitality v1.3.5 (http://themes.startbootstrap.com/vitality-v1.3.5)
+ * Copyright 2013-2015 Start Bootstrap Themes
+ * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth Scrolling: Smooth scrolls to an ID on the current page.
+    // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Activates floating label headings for the contact form.
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Owl Carousel Settings
+    $(".about-carousel").owlCarousel({
+        items: 3,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+    });
+
+    $(".portfolio-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        autoHeight: true,
+        mouseDrag: false,
+        touchDrag: false,
+        transitionStyle: "fadeUp"
+    });
+
+    $(".testimonials-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: true,
+        autoHeight: true,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        transitionStyle: "backSlide"
+    });
+
+    $(".portfolio-gallery").owlCarousel({
+        items: 3,
+    });
+
+    // Magnific Popup jQuery Lightbox Gallery Settings
+    $('.gallery-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        image: {
+            titleSrc: 'title'
+        }
+    });
+
+    // Formstone Background - Video Background Settings
+    $("header.video").background({
+        source: {
+            poster: "assets/img/title.jpg",
+            mp4: "assets/mp4/background.mp4",
+            webm: "assets/mp4/background.webm"
+        }
+    });
+
+    // Scrollspy: Highlights the navigation menu items while scrolling.
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Portfolio Filtering Scripts & Hover Effect
+    var filterList = {
+        init: function() {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap',
+                // call the hover effect
+                onMixEnd: filterList.hoverEffect()
+            });
+
+        },
+
+        hoverEffect: function() {
+
+            // Simple parallax effect
+            $('#portfoliolist .portfolio').hover(
+                function() {
+                    $(this).find('.caption').stop().animate({
+                        bottom: 0
+                    }, 200, 'easeOutQuad');
+                    $(this).find('img').stop().animate({
+                        top: -20
+                    }, 300, 'easeOutQuad');
+                },
+                function() {
+                    $(this).find('.caption').stop().animate({
+                        bottom: -75
+                    }, 200, 'easeInQuad');
+                    $(this).find('img').stop().animate({
+                        top: 0
+                    }, 300, 'easeOutQuad');
+                }
+            );
+
+        }
+
+    };
+
+    filterList.init();
+
+})(jQuery); // End of use strict
+
+// Load WOW.js on non-touch devices
+var isPhoneDevice = "ontouchstart" in document.documentElement;
+$(document).ready(function() {
+    if (isPhoneDevice) {
+        //mobile
+    } else {
+        //desktop               
+        // Initialize WOW.js
+        wow = new WOW({
+            offset: 50
+        })
+        wow.init();
+    }
+});
+
+var content_quote;
+randomNumber = Math.floor(Math.random() * 7)+1;
+if (randomNumber == 1) {
+    content_quote='"One needs atleast 8 bits to go fishing otherwise the fish will not byte //Yatin"';
+} else if (randomNumber == 2) {
+    content_quote='"Modem is a C full of opportunities //Aditya"';
+} else if(randomNumber == 3) {
+    content_quote='"Programmers are tools for converting caffeine into code //Aayush"';
+} else if(randomNumber == 4) {
+    content_quote='"Life is gonna be tough you gotta be tougher //Chetan"';
+} else if(randomNumber == 5) {
+    content_quote='"Wake up every morning with determination, sleep every night with satisfaction //Ragini"';
+} else if(randomNumber == 6) {
+    content_quote='"Every master was once a beginner //Tushar"';
+} else{
+    content_quote='"Two bytes meet. The first byte asks, “Are you ill?” The second byte replies, “No, just feeling a bit off”" //Akhil';
+}
+var divq = document.getElementById('amazing-quote');
+divq.innerHTML = divq.innerHTML + content_quote;
+
+
+$("#secret").click(function() {
+    document.getElementById('secret_key').setAttribute('href','assets/css/special.css');
+    $("body").children().each(function() {
+        $(this).html($(this).html().replace(/This is a special division placed to generate a random inspiring quote from our team./g,"Congratulations on finding the website's secret key! Here is a random quote from our team!"));
+    });
+});
